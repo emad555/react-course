86f275d5e97069258873b0a5d4197ec894e6d9a9
emadstonai <104435325+emadstonai@users.noreply.github.com>
2022-10-07 11:03:06 +0400

    all

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos';
+class App extends Component {
+  render(){
+    return (
+        <Todos />
+      );
+  }
+
+}
+
+export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+class Todos extends Component {
+  render(){
+    return (
+    <div>
+        <h1>My Portfolio!</h1>
+        <h3>Software Engineer and AI Specialist!</h3>
+        <ul>
+            <li>React</li>
+            <li>Flask</li>
+            <li>TensorFlow</li>
+        </ul>
+    </div>
+      );
+  }
+
+}
+
+export default Todos;
